2539c581bca91dab5c88bc10abb2f73687dc4477
achinchurreta <augustochinchurreta@gmail.com>
2026-05-26 14:24:07 -0600

    Add WhatsApp floating contact button

---
+++ src/constants/contact.ts
@@ -0,0 +1,16 @@
+export const CONTACT = {
+  phone: "9931336447",
+
+  whatsapp: "https://wa.me/529931336447",
+
+  email: "ceo@forenseia.org",
+
+  facebook:
+    "https://www.facebook.com/profile.php?id=61578822855300",
+
+  instagram:
+    "https://www.instagram.com/forenseia/",
+
+  tiktok:
+    "https://www.tiktok.com/@gnzlo33",
+};
